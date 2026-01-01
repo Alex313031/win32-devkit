6d7414ff8b9acdf05cbf58192f22792edc0e91dd
Alexander Frick <Alex313031@gmail.com>
2026-01-01 01:32:13 -0600

    add build bash scripts

---
+++ build_x64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copyright (c) 2025 Alex313031
+
+YEL='\033[1;33m' # Yellow
+CYA='\033[1;96m' # Cyan
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
+
+export HERE=${PWD}
+
+docker build -t w64devkit -f ${HERE}/Dockerfile ${HERE} &&
+
+docker run --rm w64devkit > ${HERE}/win32devkit_x64.exe
+
